7c24e2420f44a29cc8a70b123a7c035551429a92
Rodrigo <rodcjoao@gmail.com>
2026-02-02 02:47:49 -0300

    Feat: bash script and reading from github repo

--- scripts/juiz_sabia.py
+++ scripts/juiz_sabia.py
@@ -5,16 +5,8 @@ from pydantic import BaseModel
 import csv
 import json
 import yaml
-
-load_dotenv()
-
-SABIA_API_KEY=os.getenv("SABIA_API_KEY")
-client = OpenAI(
-    api_key=SABIA_API_KEY,
-    base_url="https://chat.maritaca.ai/api",
-)
-temp = [0.0, 0.5, 0.9]
-num_runs = 0 # DEFINIR NÚMERO DE EXECUÇÕES POR PROMPT/TEMPERATURA
+import requests
+import argparse
 
 class respostaPorCriterio(BaseModel):
   nota_1A: float
@@ -34,31 +26,55 @@ class respostaEmFaixa(BaseModel):
   faixa: str
   numero_de_erros_gramaticais: int
   erros_gramaticais: list[str]
-  feedbacks: list[str] 
+  feedbacks: list[str]
 
-jsonGerado = []
-root_path = os.getcwd()
+def main(n_iteracoes, temps, anos, lista_prompts):
+  url = "https://raw.githubusercontent.com/MeLLL-UFF/diplomatrixbr-gen/main/Diplomatrix.json"
+  requisicao = requests.get(url)
 
-path_prompts = os.path.join(root_path, "prompt_testing", "prompts.yaml")
-path_essays = os.path.join(root_path, "prompt_testing", "sheets", "redacoes2022.csv")
+  if requisicao.status_code == 200:
+    diplomatrix = requisicao.json()
+  else:
+    raise ValueError("Não foi possível acessar o repositório")
 
-with open(path_prompts, 'r', encoding='utf-8') as file:
-    prompts_yaml = yaml.safe_load(file)
-    prompts = prompts_yaml['prompts']
+  load_dotenv()
 
-with open(path_essays, encoding='utf-8') as arquivo_referencia:
+  SABIA_API_KEY=os.getenv("SABIA_API_KEY")
+  client = OpenAI(
+      api_key=SABIA_API_KEY,
+      base_url="https://chat.maritaca.ai/api",
+  )
 
-  tabela = csv.reader(arquivo_referencia, delimiter='|')
+  #temp = [0.0, 0.5, 0.9]
+  temp = temps
+  num_runs = n_iteracoes # DEFINIR NÚMERO DE EXECUÇÕES POR PROMPT/TEMPERATURA
+  #ano = "2013"
+  ano = anos
+
+  jsonGerado = []
+  root_path = os.getcwd()
+
+  path_prompts = os.path.join(root_path, "prompt_testing", "prompts.yaml")
+  #path_essays = os.path.join(root_path, "prompt_testing", "sheets", "redacoes2022.csv")
+
+  with open(path_prompts, 'r', encoding='utf-8') as file:
+      prompts_yaml = yaml.safe_load(file)
+      prompts = prompts_yaml['prompts']
+
+  #with open(path_essays, encoding='utf-8') as arquivo_referencia:
+
+  #tabela = csv.reader(arquivo_referencia, delimiter='|')
+  dados_candidatos = diplomatrix["Candidates_Essays"][ano]["Candidates"]
   numero_redacao = 0
 
-  for linha in tabela:
+  for candidato in dados_candidatos:
     numero_redacao += 1
-    redacao = linha[0]
+    redacao = candidato["Essay"]
 
     for prompt in prompts:
 
       # DEFINIR AQUI QUAIS PROMPTS SERÃO TESTADOS
-      if prompt['id'] >= 7 and prompt['id'] <= 12:
+      if prompt['id'] in lista_prompts:
         prompt_formatado = prompt['prompt'] + redacao
         prompt_formatado += "\n\n" + prompt['extras'] if 'extras' in prompt else ''
 
@@ -98,19 +114,32 @@ with open(path_essays, encoding='utf-8') as arquivo_referencia:
               response['temp'] = i
               response['versao'] = j + 1
               response['essay'] = numero_redacao
+              response['ano'] = ano
               jsonGerado.append(response)
               print(f"Temperatura testada: {i}")
+              2
             except Exception as e:
               print(f"Erro na Redação {numero_redacao} - Prompt {prompt['id']} - Temp {i} - Run {j+1}: {e}")
     
     # Salvar resultados parciais por redação pra evitar perda de dados
     # Pode ser removido se não for necessário
-    output_path = os.path.join(os.getcwd(), "prompt_testing", f'redacao_{numero_redacao}_output_{response["modelo"]}_p7-12_{num_runs}r.json')
+    output_path = os.path.join(os.getcwd(), "prompt_testing", "essay_dump", f'redacao_{ano}_prompt{prompt['id']}_{numero_redacao}_output_{response["modelo"]}_p7-12_{num_runs}r.json')
     with open(output_path, 'w', encoding="utf-8") as file:
       json.dump(jsonGerado, file, indent=2, ensure_ascii=False)
       file.close()
 
-  output_path = os.path.join(os.getcwd(), "prompt_testing", f'output_{response["modelo"]}_p7-12_{num_runs}r.json')
+  output_path = os.path.join(os.getcwd(), "prompt_testing", f'output_{response["modelo"]}_p12_{num_runs}r.json')
   with open(output_path, 'w', encoding="utf-8") as file:
     json.dump(jsonGerado, file, indent=2, ensure_ascii=False)
     file.close()
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Avalia redações de um determinado ano do CACD com Sabia-3.")
+  parser.add_argument("--n_iteracoes", type=int, required=True, help="Quantas iterações por redação.")
+  parser.add_argument("--temps", type=str, nargs="+", required=True, help="Temperaturas usadas.")
+  parser.add_argument("--anos", type=str, required=True, help="Anos avaliados.")
+  parser.add_argument("--prompts", type=int, nargs="+", required=True, help="Prompts testados.")
+
+  args = parser.parse_args()
+
+  main(args.n_iteracoes, args.temps, args.anos, args.prompts)
