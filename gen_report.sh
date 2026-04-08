ff228c18a74413dce2ac59642e049bc7890eaab4
Rodrigo <rodcjoao@gmail.com>
2026-04-07 22:29:44 -0300

    Fix: Fixing plot

--- gen_report.sh
+++ gen_report.sh
@@ -1,5 +1,5 @@
-YEAR="2022"
-EVALPATH="prompt_testing\sheets\FIX_sabia_${YEAR}_p7-12_3r.csv"
+YEAR="2018"
+EVALPATH="prompt_testing\sheets\sabia_${YEAR}_p7-12_3r.csv"
 HUMANPATH="prompt_testing\sheets\notas_humanas_${YEAR}.csv"
 
 python -m scripts.analysis_report --num_runs 3 --eval_path $EVALPATH --human_path $HUMANPATH --model sabia --model_version 3.1 --year $YEAR
