16f929384132b8ead0387de51d1da48cff5f1ce8
rodcjoao <rodcjoao@gmail.com>
2026-05-13 06:11:40 -0300

    Adding hugging_face gens

--- eval_hugging.sh
+++ eval_hugging.sh
@@ -1,7 +1,10 @@
-NITERACOES="1"
-TEMPS="0.3"
-PROMPTS="8"
-ESSAY="1"
-NOME_MODELO="google/gemma-3-1b-it"
+NITERACOES="3"
+TEMPS="0.0"
+PROMPTS="7 8 9"
+ESSAY=""
+NOME_MODELO="Qwen/Qwen3.6-35B-A3B"
 
-python -m scripts.juiz_hugging --n_iteracoes $NITERACOES --temps $TEMPS --anos 2022 --prompts $PROMPTS --nome_modelo $NOME_MODELO --redacoes $ESSAY
+python3 -m scripts.juiz_hugging --n_iteracoes $NITERACOES --temps $TEMPS --anos 2019 --prompts $PROMPTS --nome_modelo $NOME_MODELO
+python3 -m scripts.juiz_hugging --n_iteracoes $NITERACOES --temps $TEMPS --anos 2020-2021 --prompts $PROMPTS --nome_modelo $NOME_MODELO
+python3 -m scripts.juiz_hugging --n_iteracoes $NITERACOES --temps $TEMPS --anos 2022 --prompts $PROMPTS --nome_modelo $NOME_MODELO
+python3 -m scripts.juiz_hugging --n_iteracoes $NITERACOES --temps $TEMPS --anos 2023 --prompts $PROMPTS --nome_modelo $NOME_MODELO
