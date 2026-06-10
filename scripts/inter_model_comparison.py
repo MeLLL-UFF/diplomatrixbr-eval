dab0feb120eee34b8ff6a9e201c5d607026f2d13
Rodrigo Cavalcanti <rodrigocavalcanti@Mac-mini-de-ic-2.local>
2026-06-10 12:09:50 -0300

    Adding scatterplots

--- scripts/inter_model_comparison.py
+++ scripts/inter_model_comparison.py
@@ -32,7 +32,7 @@ def real_prompt_name(prompt_name):
 
 def inter_model_correlation(complete_df, model_list):
     lista_prompts = complete_df["prompt"].unique()
-    fig, axes = plt.subplots(len(lista_prompts), 2, figsize=(12, 16))
+    fig, axes = plt.subplots(len(lista_prompts), 2, figsize=(10, 10))
 
     scaler = MinMaxScaler()
     for prompt in range(len(lista_prompts)):
@@ -49,21 +49,21 @@ def inter_model_correlation(complete_df, model_list):
         df_correlacao_pearson = df_normalizado.corr(method="pearson")
         df_correlacao_spearman = df_normalizado.corr(method="spearman")
 
-        axes[prompt, 0] = sns.heatmap(df_correlacao_pearson, annot=True, vmax=1, vmin=-1, cmap="RdYlGn", fmt=".2f", ax=axes[prompt, 0], cbar=False)
+        axes[prompt, 0] = sns.heatmap(df_correlacao_pearson, annot=True, vmax=1, vmin=-1, cmap="RdYlGn", fmt=".2f", ax=axes[prompt, 0], cbar=True)
         axes[prompt, 0].xaxis.tick_top()
         axes[prompt, 0].set_title(f"Pearson \n {real_prompt_name(lista_prompts[prompt])}")
         axes[prompt, 1] = sns.heatmap(df_correlacao_spearman, annot=True, vmax=1, vmin=-1, cmap="RdYlGn", fmt=".2f", ax=axes[prompt, 1], cbar=True)
         axes[prompt, 1].xaxis.tick_top()
         axes[prompt, 1].set_title(f"Spearman \n {real_prompt_name(lista_prompts[prompt])}")
 
-    plt.subplots_adjust(hspace=0.3)
+    plt.subplots_adjust(wspace=0.175, hspace=0.4)
 
-    plt.suptitle("Correlações Inter-Modelos", fontsize=30.0)
+    plt.suptitle("Correlações Inter-Modelos", fontsize=15.0)
     plt.savefig(os.path.join("prompt_testing", "reports", "Correlacao_inter_modelos.png"))
 
 def inter_model_QWK(complete_df, model_list, bins:int=5):
     lista_prompts = complete_df["prompt"].unique()
-    fig, axes = plt.subplots(len(lista_prompts), 1, figsize=(6, 16))
+    fig, axes = plt.subplots(len(lista_prompts), 1, figsize=(5, 9))
 
     for prompt in range(len(lista_prompts)):
         temp_df = complete_df[complete_df["prompt"] == lista_prompts[prompt]]
@@ -86,13 +86,13 @@ def inter_model_QWK(complete_df, model_list, bins:int=5):
             
             kappa_df.insert(len(kappa_df.columns), model_name_abbreviation(model_ref), kappa_per_model)
 
-        axes[prompt] = sns.heatmap(kappa_df, annot=True, vmax=1, vmin=-1, cmap="RdYlGn", ax=axes[prompt])
+        axes[prompt] = sns.heatmap(kappa_df, annot=True, vmax=1, vmin=-1, cmap="RdYlGn", ax=axes[prompt], fmt=".2f")
         axes[prompt].set_title(f"{real_prompt_name(lista_prompts[prompt])}")
         axes[prompt].xaxis.tick_top()
 
     plt.subplots_adjust(hspace=0.3)
 
-    plt.suptitle("QWK Inter-Modelos", fontsize=30.0)
+    plt.suptitle("QWK Inter-Modelos", fontsize=12.0)
     plt.savefig(os.path.join("prompt_testing", "reports", "QWK_inter_modelos.png"))
 
 def inter_model_boxplot(complete_df):
@@ -106,13 +106,71 @@ def inter_model_boxplot(complete_df):
 
     sns.boxplot(complete_df, x="judge", y="nota_final", hue="prompt", showfliers=False, palette=["#FF6B6B", "#11F9DE", "#FFD918", "#A3FF22"])
 
-    ax.set_yticks(range(20, 71, 2.5))
+    ax.set_yticks(range(20, 71, 5))
     ax.set_xlabel("Avaliadores")
     ax.set_ylabel("Notas")
     sns.move_legend(ax, loc="best", title="")
     plt.suptitle("Boxplot das notas por modelo")
     plt.savefig(os.path.join("prompt_testing", "reports", "Boxplot_inter_modelos.png"))
         
+def inter_model_error_scatterplot(complete_df):
+    fig, ax = plt.subplots(figsize=(8, 8))
+
+    human_df = get_full_human_df(os.path.join(os.getcwd(), "prompt_testing", "sheets", "notas_humanas"))
+    complete_df = complete_df.merge(human_df, on=["redacao"], how='inner', suffixes=("_model", "_human"))
+
+    complete_df["erro"] = complete_df["nota_final_model"] - complete_df["nota_final_human"]
+
+    complete_df = complete_df.loc[abs(complete_df["erro"]) < 100]
+
+    complete_df["judge_model"] = complete_df["judge_model"].apply(model_name_abbreviation)
+    complete_df["prompt"] = complete_df["prompt"].apply(real_prompt_name)
+
+    complete_df = complete_df.rename(columns={"judge_model":"Modelos", "prompt":"Prompts"})
+
+    #ax.set_yticks(range(-max_value, max_value+1, 5))
+    sns.scatterplot(complete_df, x="nota_final_human", y="erro", hue="Modelos", style="Prompts", markers=["o", "X", "D"], palette=["#FF9D00", "#61ADFF", "#BB0000", "#800B9B", "#2F8600"])
+    sns.move_legend(ax, loc="best", title="")
+    ax.set_ylim(-41, 30)
+    ax.set_xlim(30, 70.5)
+    ax.set_xlabel("Notas Humanas")
+    ax.set_ylabel("Erro")
+
+    plt.axhline(y=0, color='black', linestyle='dashed')
+
+    plt.suptitle("Scatterplot do erro dos modelos")
+    plt.savefig(os.path.join("prompt_testing", "reports", "Scatterplot_inter_modelos_erro.png"))
+
+def inter_model_scores_scatterplot(complete_df):
+    fig, ax = plt.subplots(figsize=(8, 8))
+
+    human_df = get_full_human_df(os.path.join(os.getcwd(), "prompt_testing", "sheets", "notas_humanas"))
+    complete_df = complete_df.merge(human_df, on=["redacao"], how='inner', suffixes=("_model", "_human"))
+
+    complete_df["erro"] = complete_df["nota_final_model"] - complete_df["nota_final_human"]
+
+    complete_df = complete_df.loc[complete_df["nota_final_model"] > 30]
+
+    complete_df["judge_model"] = complete_df["judge_model"].apply(model_name_abbreviation)
+    complete_df["prompt"] = complete_df["prompt"].apply(real_prompt_name)
+
+    complete_df = complete_df.rename(columns={"judge_model":"Modelos", "prompt":"Prompts"})
+
+    #ax.set_yticks(range(-max_value, max_value+1, 5))
+    sns.scatterplot(complete_df, x="nota_final_human", y="nota_final_model", hue="Modelos", style="Prompts", markers=["o", "X", "D"], palette=["#FF9D00", "#61ADFF", "#BB0000", "#800B9B", "#2F8600"])
+    sns.move_legend(ax, loc="best", title="", fontsize="x-small")
+    ax.set_ylim(30, 70.5)
+    ax.set_xlim(30, 70.5)
+    ax.set_xticks(range(30, 71, 5))
+    ax.set_xlabel("Notas Humanas")
+    ax.set_ylabel("Notas Modelos")
+
+    afim = np.linspace(30, 70.5, 200)
+    ax.plot(afim, afim, color="black", linestyle="dashed")
+
+    plt.suptitle("Scatterplot das notas dos modelos")
+    plt.savefig(os.path.join("prompt_testing", "reports", "Scatterplot_inter_modelos_score.png"))
+
 def main(model_list, num_runs):
     models_dfs = []
     for model in model_list:
@@ -135,7 +193,9 @@ def main(model_list, num_runs):
     #inter_model_QWK(complete_df, model_list, 5)
 
     complete_df = complete_df[complete_df["nota_final"] > 0]
-    inter_model_boxplot(complete_df)
+    #inter_model_boxplot(complete_df)
+    inter_model_error_scatterplot(complete_df)
+    inter_model_scores_scatterplot(complete_df)
 
 
 if __name__ == "__main__":
